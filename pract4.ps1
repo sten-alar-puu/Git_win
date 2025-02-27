0322dd27b1449b10852b2f23beebc366f978dbe7
sten-alar-puu <sten-alar.puusepp@voco.ee>
2025-02-27 11:21:59 +0200

    harjutus4

---
+++ pract4.ps1
@@ -0,0 +1,11 @@
+﻿$value1 = Read-Host "Give number"
+$value2 = Read-Host "Give number"
+
+if($value1 -gt $value2)
+{
+Write-Host "The higher number is : "$value1
+}
+else
+{
+Write-Host "The higher number is : "$value2
+}
